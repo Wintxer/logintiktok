0a321414355c00fbc852f96f9c25c9fad21192d5
Wintxer <47366828+Wintxer@users.noreply.github.com>
2022-12-12 15:37:10 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gimucco/tiktok-loginkit' => array(
+            'pretty_version' => 'v0.4.1',
+            'version' => '0.4.1.0',
+            'reference' => '11c866a547b77c0a779efb6093084f10c5aa2307',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gimucco/tiktok-loginkit',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
